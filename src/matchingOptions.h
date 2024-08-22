6358c37f9efd5027d475e3da09f6573cb721a211
eric-14 <ericmwas01@gmail.com>
2024-08-22 22:58:52 +0300

    remove bug1

--- src/matchingOptions.h
+++ src/matchingOptions.h
@@ -6,7 +6,7 @@ class matchingOptions
 private:
     //supported options in grep 
     //map stores the options parameter and a function pointer to implement the function call 
-   std::map<std::string, bool (*func)(const std::string &inputline, const std::string &pattern)> matchController;
+   std::map<std::string, bool (*)(const std::string &inputline, const std::string &pattern)> matchController;
 
 public:
     //function to match digit 
