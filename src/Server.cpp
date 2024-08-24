1ffb350b44e4c1a7df09c393e756abb6cce957a4
eric-14 <ericmwas01@gmail.com>
2024-08-25 00:46:59 +0300

    Refactor 3

--- src/Server.cpp
+++ src/Server.cpp
@@ -55,6 +55,7 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {        
         return input_line.find(pattern) != std::string::npos;
     }else {
+        std::cout<<"line 58 the pattern is "<<pattern<<std::endl;
         throw std::runtime_error("Unhandled pattern " + pattern);
        
     }
