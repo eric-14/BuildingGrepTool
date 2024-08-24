d6a007e35af67ce177c267f2950b3724eac85b97
eric-14 <ericmwas01@gmail.com>
2024-08-24 23:41:14 +0300

    alphanumeric

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,7 +3,7 @@
 #include <regex>
 
 bool matchingFn(const std::string &line, const std::string &pattern){
-    std::regex reg("^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$");
+    std::regex reg("^[A-Za-z0-9]+$");
     if(pattern == "\\d"){                   
         int value = std::stoi(line); // try to convert string to int
         std::cout<<"The value of the function is "<<value<<std::endl;
